6a750aee142058a1606f2d9a73a5ab9ea4d6fac5
飯田翔一 <135586613+shoichi-iida@users.noreply.github.com>
2026-06-25 12:53:59 +0900

    環境変数によるDB定義のオーバーライド対応

--- functions/common/control_db.py
+++ functions/common/control_db.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import os
 from glob import glob
 from tornado.options import options
 from functions.define.base_define import BaseDefine
 from functions.data.control_tinydb import ControlTinyDB
 from functions.data.control_sqlite import ControlSqlite
 try:
-        from functions.data.control_mysql import ControlMysql
+	from functions.data.control_mysql import ControlMysql
 except:
-        pass
+	pass
 
 class ControlDB():
 	"""
@@ -17,7 +18,7 @@ class ControlDB():
 	def __init__(self, ctrl_define):
 		"""
 		コンストラクタ
-		
+
 		Parameters
 		----------
 		ctrl_define : functions.define.base_define.BaseDefine
@@ -26,6 +27,7 @@ class ControlDB():
 		self.ctrl_db = {}
 		def_db = ctrl_define["database"]["def"]
 		for db in def_db.values():
+			db = self.__apply_env_overrides(db)
 			if db["db_type"] == "tinydb":
 				if db.get("inmemory", "0") == "1":
 					self.ctrl_db[db["id"]] = ControlTinyDB(None)
@@ -35,8 +37,8 @@ class ControlDB():
 				self.ctrl_db[db["id"]] = ControlMysql(
 					db["db_host"],
 					int(db.get("db_port", "3306")),
-					db["db_user"], 
-					db["db_password"], 
+					db["db_user"],
+					db["db_password"],
 					db["db_name"],
 					db["db_charset"]
 				)
@@ -66,3 +68,43 @@ class ControlDB():
 		DB取得プロパティ
 		"""
 		return self.ctrl_db.get(key, None)
+
+	def __apply_env_overrides(self, db):
+		"""
+		環境変数によるDB定義のオーバーライド処理
+	
+		database.xmlの各DB定義に対して、環境変数が設定されている場合はその値で上書きする。
+		環境変数の命名規則: {DB_ID(大文字)}_{属性名(大文字)}
+		例: db_control → DB_CONTROL_TYPE, DB_CONTROL_HOST, ...
+	
+		Parameters
+		----------
+		db : dict
+			DB定義辞書
+	
+		Returns
+		-------
+		dict
+			環境変数で上書きされたDB定義辞書
+		"""
+		prefix = db["id"].upper()
+		env_map = {
+			"db_type":    f"{prefix}_TYPE",
+			"db_host":    f"{prefix}_HOST",
+			"db_port":    f"{prefix}_PORT",
+			"db_user":    f"{prefix}_USER",
+			"db_password": f"{prefix}_PASSWORD",
+			"db_name":    f"{prefix}_NAME",
+			"db_charset": f"{prefix}_CHARSET",
+			"db_path":    f"{prefix}_PATH",
+			"inmemory":   f"{prefix}_INMEMORY",
+		}
+		for attr, env_key in env_map.items():
+			val = os.environ.get(env_key)
+			if val is not None:
+				db[attr] = val
+		return db
+	
+	def keys(self):
+		return self.ctrl_db.keys()
+	
